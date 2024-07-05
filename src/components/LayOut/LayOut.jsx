ed18148239e262fa46e21ffd4e9a53996819827c
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-07-05 20:40:28 +0100

    just commit - music application

--- src/components/LayOut/LayOut.jsx
+++ src/components/LayOut/LayOut.jsx
@@ -1,29 +1,29 @@
-// import { useState, useEffect } from "react";
-// import songs from "../../data.json";
-// import LeftSide from "../LeftSide/LeftSide";
-// import RightSide from "../RightSide/RightSide";
-// import "./LayOut.css";
-// import { getToken, getTrackInfo } from "../../spotifyAPI";
-// import Loader from "../Loader/Loader";
+import { useState, useEffect } from "react";
+import songs from "../../data.json";
+import LeftSide from "../LeftSide/LeftSide";
+import RightSide from "../RightSide/RightSide";
+import "./LayOut.css";
+import { getToken, getTrackInfo } from "../../spotifyAPI";
+import Loader from "../Loader/Loader";
 
-// const LayOut = () => {
-//   const [tracks, setTracks] = useState([]);
+const LayOut = () => {
+  const [tracks, setTracks] = useState([]);
 
-//   useEffect(() => {
-//     getToken().then((response) => {
-//       getTrackInfo(response.access_token).then((profile) => {
-//         console.log(profile?.tracks);
-//         setTracks(profile?.tracks);
-//       });
-//     });
-//   }, []);
+  useEffect(() => {
+    getToken().then((response) => {
+      getTrackInfo(response.access_token).then((profile) => {
+        console.log(profile?.tracks);
+        setTracks(profile?.tracks);
+      });
+    });
+  }, []);
 
-//   return (
-//     <div className="layout">
-//       <LeftSide songs={tracks} />
-//       <RightSide songs={tracks} />
-//     </div>
-//   );
-// };
+  return (
+    <div className="layout">
+      <LeftSide songs={tracks} />
+      <RightSide songs={tracks} />
+    </div>
+  );
+};
 
-// export default LayOut;
+export default LayOut;
