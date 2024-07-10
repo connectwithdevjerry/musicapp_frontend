26c7bcbc0347dde9c5eea6908987a1a691e6b7cb
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-07-10 20:43:24 +0100

    just commit - music application

--- src/components/ListSongs/ListSongs.jsx
+++ src/components/ListSongs/ListSongs.jsx
@@ -1,29 +1,29 @@
-// import React from "react";
-// import "./ListSongs.css";
-// import { mergeArtist } from "../../helper";
+import React from "react";
+import "./ListSongs.css";
+import { mergeArtist } from "../../helper";
 
-// const ListSongs = ({ songs }) => {
-//   return (
-//     <div className="list-music">
-//       {songs.map((song, index) => (
-//         <div key={index} className="loaders">
-//           <div className="songs update-song">
-//             <div className="first">
-//               <p className="names update-name">{song.name}</p>
-//             </div>
-//             <div className="sec">
-//               <p className="artist">#{song.popularity} plays</p>
-//             </div>
-//             <div className="third">
-//               <p className="artist">{mergeArtist(song.artists)}</p>
-//             </div>
-//           </div>
-//           <div className="albumcover"></div>
-//           <div className="play"></div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+const ListSongs = ({ songs }) => {
+  return (
+    <div className="list-music">
+      {songs.map((song, index) => (
+        <div key={index} className="loaders">
+          <div className="songs update-song">
+            <div className="first">
+              <p className="names update-name">{song.name}</p>
+            </div>
+            <div className="sec">
+              <p className="artist">#{song.popularity} plays</p>
+            </div>
+            <div className="third">
+              <p className="artist">{mergeArtist(song.artists)}</p>
+            </div>
+          </div>
+          <div className="albumcover"></div>
+          <div className="play"></div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-// export default ListSongs;
+export default ListSongs;
