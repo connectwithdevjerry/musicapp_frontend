e5f9d92ac44f05e07b8af98eaf1fe32dd9b8c43c
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-07-30 20:53:22 +0100

    just commit - music application

--- src/components/UpdateProfile/UpdateProfile.jsx
+++ src/components/UpdateProfile/UpdateProfile.jsx
@@ -1,36 +1,36 @@
-// import React from "react";
-// import "./UpdateProfile.css";
-// import { Link } from "react-router-dom";
-// const UpdateProfile = () => {
-//   return (
-//     <>
-//       <div class="form-container">
-//         <form class="form">
-//           <span>Update your Information</span>
-//           <div class="form-group">
-//             <label for="email">Company Email</label>
-//             <input type="text" id="email" name="email" required="" />
-//           </div>
-//           <div class="form-group">
-//             <label for="textarea">How Can We Help You?</label>
-//             <textarea
-//               name="textarea"
-//               id="textarea"
-//               rows="10"
-//               cols="50"
-//               required=""
-//             >
-//               {" "}
-//             </textarea>
-//           </div>
-//           <button class="form-submit-btn" type="submit">
-//             Submit
-//           </button>
-//         </form>
-//       </div>
-//       <Link to="/signin">sign out</Link>
-//     </>
-//   );
-// };
+import React from "react";
+import "./UpdateProfile.css";
+import { Link } from "react-router-dom";
+const UpdateProfile = () => {
+  return (
+    <>
+      <div class="form-container">
+        <form class="form">
+          <span>Update your Information</span>
+          <div class="form-group">
+            <label for="email">Company Email</label>
+            <input type="text" id="email" name="email" required="" />
+          </div>
+          <div class="form-group">
+            <label for="textarea">How Can We Help You?</label>
+            <textarea
+              name="textarea"
+              id="textarea"
+              rows="10"
+              cols="50"
+              required=""
+            >
+              {" "}
+            </textarea>
+          </div>
+          <button class="form-submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+      <Link to="/signin">sign out</Link>
+    </>
+  );
+};
 
-// export default UpdateProfile;
+export default UpdateProfile;
