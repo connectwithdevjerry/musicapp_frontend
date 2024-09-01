5d51357fa65a0568cac0d1b3965ec307eb8c958b
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-01 21:09:55 +0100

    just commit - music application

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 
-// import LayOut from "../components/LayOut/LayOut";
+import LayOut from "../components/LayOut/LayOut";
 
 
-// const HomePage = () => {
-//   return (
-//     <>
-//       <LayOut />
-//     </>
-//   );
-// };
+const HomePage = () => {
+  return (
+    <>
+      <LayOut />
+    </>
+  );
+};
 
-// export default HomePage;
+export default HomePage;
