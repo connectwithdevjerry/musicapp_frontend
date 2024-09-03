82ce93b46d92d885dc65bb81ce62ca8f7e1b36c0
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-03 21:11:02 +0100

    just commit - music application

--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,12 @@
-// export const mergeArtist = (artists) => {
-//   if (artists.length > 1) {
-//     var showArtiste = "";
-//     for (i = 0; i < artists.length; i++) {
-//       showArtiste += artists[i].name + ", ";
-//     }
-//     let merged = showArtiste.slice(0, showArtiste.length);
-//     return merged;
-//   } else {
-//     return artists[0].name;
-//   }
-// };
+export const mergeArtist = (artists) => {
+  if (artists.length > 1) {
+    var showArtiste = "";
+    for (i = 0; i < artists.length; i++) {
+      showArtiste += artists[i].name + ", ";
+    }
+    let merged = showArtiste.slice(0, showArtiste.length);
+    return merged;
+  } else {
+    return artists[0].name;
+  }
+};
