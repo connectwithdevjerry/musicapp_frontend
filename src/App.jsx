2ff8c58f1e715e0bfe82af1549f78d4d5b99bb98
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-04 21:11:37 +0100

    just commit - music application

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-// import { useState } from "react";
-// import NavBar from "./components/NavBar/NavBar";
-// import SignIn from "./components/SignIn/SignIn";
-// import HomePage from "./pages/HomePage";
-// import MainLayout from "./Layouts/MainLayout";
-// import SignUp from "./components/SignUp/SignUp";
-// import {
-//   Route,
-//   createBrowserRouter,
-//   createRoutesFromElements,
-//   RouterProvider,
-// } from "react-router-dom";
-// import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-// import SetProfile from "./pages/SetProfile/SetProfile";
-// import LayOut from "./components/LayOut/LayOut";
-// import Payments from "./pages/Payments/Payments";
+import { useState } from "react";
+import NavBar from "./components/NavBar/NavBar";
+import SignIn from "./components/SignIn/SignIn";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./Layouts/MainLayout";
+import SignUp from "./components/SignUp/SignUp";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import SetProfile from "./pages/SetProfile/SetProfile";
+import LayOut from "./components/LayOut/LayOut";
+import Payments from "./pages/Payments/Payments";
 
-// function App() {
-//   const router = createBrowserRouter(
-//     createRoutesFromElements(
-//       <Route path="/" element={<MainLayout />}>
-//         <Route index element={<HomePage />} />
-//         <Route path="/signin" element={<SignIn />} />
-//         <Route path="/signup" element={<SignUp />} />
-//         <Route path="/payment" element={<Payments />} />
-//         <Route path="/profile/:id" element={<SetProfile />} />
-//         <Route path="*" element={<NotFoundPage />} />
-//       </Route>
-//     )
-//   );
-//   return <RouterProvider router={router} />;
-// }
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/payment" element={<Payments />} />
+        <Route path="/profile/:id" element={<SetProfile />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+}
 
-// export default App;
+export default App;
